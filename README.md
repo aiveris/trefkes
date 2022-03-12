f00e082d68f6cc0a912a6a50e8e8ecb501c8f8cd
aiveris@gmail.com <Staind0323>
2022-03-12 17:05:06 +0200

    2022-03-12

---
+++ README.md
@@ -0,0 +1,4 @@
+# kasis
+
+App Kasis is registration form for basketball players with Firebase storage.
+After name input appears edit and delete buttons for manage data.
---
+++ js/script.js
@@ -0,0 +1,99 @@
+"use strict";
+const addTodoA1 = document.querySelector(".add-todo-a1");
+const addTodoFormA1 = document.querySelector(".add-todo-a1 .form");
+const tableTodosA1 = document.querySelector(".table-a1");
+const editModal = document.querySelector(".edit-modal");
+const editModalForm = document.querySelector(".edit-modal .form");
+const btnAdd = document.querySelector(".btn-add");
+const plansA1 = document.querySelector(".buttonA1");
+let id;
+let countA1 = 0;
+
+// Create element and render to-do a1 ----------------------------------
+const renderTodoA1 = (doc) => {
+  const tr = `
+    <tr data-id='${doc.id}'>
+      <td>${doc.data().todo}</td>
+      <th>        
+        <i
+        class="fa fa-edit btn-edit"
+        onclick="close_modal()"
+        style="font-size: 24px"
+      ></i>
+        <i
+        class="fa fa-trash btn-delete"
+        onclick="close_modal()"
+        style="font-size: 24px"
+      ></i>
+      </th>
+    </tr>
+  `;
+  tableTodosA1.insertAdjacentHTML("beforeend", tr);
+  // Click edit to-do
+  const btnEdit = document.querySelector(`[data-id='${doc.id}'] .btn-edit`);
+  btnEdit.addEventListener("click", () => {
+    editModal.classList.add("modal-show");
+    id = doc.id;
+    editModalForm.todo.value = doc.data().todo;
+  });
+  // Click delete to-do
+  const btnDelete = document.querySelector(`[data-id='${doc.id}'] .btn-delete`);
+  btnDelete.addEventListener("click", () => {
+    db.collection("a1").doc(`${doc.id}`).delete();
+  });
+};
+// // Real time listener
+db.collection("a1").onSnapshot((snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    if (change.type === "added") {
+      renderTodoA1(change.doc);
+      countA1++;
+      document.getElementById("countA1").innerHTML = countA1;
+    }
+    if (change.type === "removed") {
+      let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+      let tbody = tr.parentElement;
+      tableTodosA1.removeChild(tbody);
+      countA1--;
+      document.getElementById("countA1").innerHTML = countA1;
+    }
+    if (change.type === "modified") {
+      let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+      let tbody = tr.parentElement;
+      tableTodosA1.removeChild(tbody);
+      renderTodoA1(change.doc);
+    }
+  });
+});
+addTodoFormA1.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("a1").add({
+    todo: addTodoFormA1.todo.value,
+  });
+  addTodoFormA1.todo.value = "";
+});
+// Click submit in edit to-do
+editModalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  db.collection("a1").doc(id).update({
+    todo: editModalForm.todo.value,
+  });
+  editModal.classList.remove("modal-show");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === addTodoA1) {
+    addModal.classList.remove("modal-show");
+  }
+  if (e.target === editModal) {
+    editModal.classList.remove("modal-show");
+  }
+});
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Escape" || event.code === 27) {
+    editModal.classList.remove("modal-show");
+  }
+});
+function close_modal() {
+  editModal.classList.remove("modal-show");
+}
